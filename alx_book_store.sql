5be0f13c8243f41973a33eccf3b3111bd21fa59a
Hamza amine amil <hamza.amineamil@gmail.com>
2025-08-18 21:17:37 +0100

    changes on DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,30 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Authors(
-    author_id INT PRIMARY KEY
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 CREATE TABLE Books (
-    book_id INT PRIMARY KEY
-    title VARCHAR(130)
-    author_id FOREIGN KEY(author_id) REFERENCES Authors(author_id)
-    price DOUBLE
-    publication_date DATE
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
 
 );
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY
-    customer_id FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-    order_date DATE
+    order_id INT PRIMARY KEY,
+    customer_id,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY
-    order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id)
-    book_id FOREIGN KEY (book_id) REFERENCES Books(book_id)
-    quantity DOUBLE
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
