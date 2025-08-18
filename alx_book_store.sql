b18d4af5c38c87ca493b773c5e313fbb001cf361
Hamza amine amil <hamza.amineamil@gmail.com>
2025-08-18 01:58:40 +0100

    add SQL file V1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,30 +1,30 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Authors(
-    author_id PRIMARY KEY
+    author_id INT PRIMARY KEY
     author_name VARCHAR(215)
 );
 CREATE TABLE Books (
     book_id INT PRIMARY KEY
     title VARCHAR(130)
-    author_id FOREIGN KEY
+    author_id FOREIGN KEY(author_id) REFERENCES Authors(author_id)
     price DOUBLE
     publication_date DATE
 
 );
 CREATE TABLE Customers (
-    customer_id Primary Key
+    customer_id INT PRIMARY KEY
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
 );
 CREATE TABLE Orders (
-    order_id  INT Primary Key
-    customer_id Foreign Key
+    order_id  INT PRIMARY KEY
+    customer_id FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
     order_date DATE
 );
 CREATE TABLE Order_Details (
-    orderdetailid Primary Key
-    order_id Foreign Key
-    book_id Foreign Key
+    orderdetailid INT PRIMARY KEY
+    order_id FOREIGN KEY(order_id) REFERENCES Orders(order_id)
+    book_id FOREIGN KEY(book_id) REFERENCES Books(book_id)
     quantity DOUBLE
 );
